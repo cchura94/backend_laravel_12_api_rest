13264577ad9ebe35d8203c3119d07b4da2ebbde2
Cristian <cchura.cpc@gmail.com>
2026-02-02 21:12:40 -0400

    Autenticacion api Laravel

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function funRegister(Request $request){
+        // Registrar un nuevo Usuario
+        // validar
+        $request->validate([
+            "name" => "required|string|min:3|max:30",
+            "email" => "required|email",
+            "password" => "required",
+            "password2" => "required|same:password"
+        ]);
+
+        $usuario = new User();
+        $usuario->name = $request->name;
+        $usuario->email = $request->email;
+        $usuario->password = Hash::make($request->password); // bcrypt($request->password);
+        $usuario->save();
+        
+        return response()->json(["mensaje" => "Usuario Registrado"], 201);
+    }
+
+    public function funLogin(Request $request){
+        // Autenticación
+
+        // validar
+        $credenciales = $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+        
+        if(!Auth::attempt($credenciales)){
+            return response()->json(["mensaje" => "Credenciales Incorrectas"]);
+        }
+
+        // generar TOKEN
+        $token = $request->user()->createToken("TokenAuth")->plainTextToken;
+
+        return response()->json(["access_token" => $token, "usuario" => $request->user()]);
+    }
+
+    public function funProfile(Request $request){
+        // 
+        return $request->user();
+    }
+
+    public function funLogout(Request $request){
+
+    }
+}
