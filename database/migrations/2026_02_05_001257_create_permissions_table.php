a66525ef8c7737791825a6529824e2a74548424a
Cristian <cchura.cpc@gmail.com>
2026-02-04 21:14:46 -0400

    migraciones N:M roles y permisos

---
+++ database/migrations/2026_02_05_001257_create_permissions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->id();
+            $table->string("action"); // create, update,reafd, delete
+            $table->timestamps();
+            $table->string("subject"); // user, role, producto
+            
+            $table->boolean("visibleInMenu")->default(true);
+            $table->string("label")->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permissions');
+    }
+};
